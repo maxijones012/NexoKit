4a3db59c6a2d9da3da40438ed0eded370bbb8f24
xO_IronShield_Ox <maxijones012@gmail.com>
2026-09-05 13:02:31 -0300

    release: Android R10 v1.0.0

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.max.kitherramientas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.9.0"
+        versionCode = 10
+        versionName = "1.0.0"
     }
 
     buildTypes {
